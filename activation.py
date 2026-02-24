b07460e27093786e328dee756837e8b3203800d0
Lev <21levperminov@gmail.com>
2026-02-24 20:30:53 +0700

    Add Sigmoid and Softmax functions

--- activation.py
+++ activation.py
@@ -4,3 +4,13 @@ from module import Module
 class ReLU(Module):
     def forward(self, x):
         return x.relu()
+
+
+class Sigmoid(Module):
+    def forward(self, x):
+        return (1 / (1 + (-x).exp()))
+    
+
+class Softmax(Module):
+    def forward(self, x):
+        return x.exp() / x.exp().sum(axis=-1, keepdims=True)
