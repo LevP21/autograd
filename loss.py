f67c3c7168d807b8cd3c4fadbf2086c98ebaeeae
Lev <21levperminov@gmail.com>
2026-02-24 20:29:55 +0700

    Add Cross Entropy loss and Binary Cross Entropy loss

--- loss.py
+++ loss.py
@@ -1,7 +1,19 @@
 from module import Module
+from utils import clip
 
-
-class MSE(Module):
+class MSELoss(Module):
     def forward(self, pred, target):
         diff = pred - target
         return (diff * diff).mean()
+
+   
+class CrossEntropyLoss(Module):
+    def forward(self, pred, target):
+        eps = 1e-8
+        pred = clip(pred, eps, 1)
+        return - (pred.log() * target).sum(axis=-1, keepdims=True)
+    
+
+class BCELoss(Module):
+    def forward(self, pred, target):
+        return - (pred.log() * target + (1 - pred).log() * (1 - target))
